9c4b413cafcbcebe683f92b704bffad1c46122f6
ananastasiap <anestheziia@gmail.com>
2023-04-05 09:56:42 +0300

    feat: add tabs for calc modals

--- src/js/main.js
+++ src/js/main.js
@@ -13,5 +13,11 @@ window.addEventListener('DOMContentLoaded', () => {
     tabSelector: '.no_click',
     contentSelector: '.decoration_content > div > div',
     activeClass: 'after_click'});
+  tabs({
+    headerSelector: '.balcon_icons',
+    tabSelector: '.balcon_icons_img',
+    contentSelector: '.big_img > img',
+    activeClass: 'do_image_more',
+    display: 'inline-block'});
   forms();
 });
