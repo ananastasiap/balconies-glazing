a9421c389971e18766ddce6d029e46a5fc069fe3
ananastasiap <anestheziia@gmail.com>
2023-04-05 10:24:08 +0300

    feat: add data-modals / add func close all modals

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -3,12 +3,15 @@ export const modals = () => {
     const triggers = document.querySelectorAll(triggerSelector);
     const modal = document.querySelector(modalSelector);
     const close = document.querySelector(closeSelector);
+    const windows = document.querySelectorAll('[data-modal]');
 
     triggers.forEach(trigger => {
       trigger.addEventListener('click', (event) => {
         if (event.target) {
           event.preventDefault();
         }
+
+        closeAllModals();
 
         modal.style.display = 'block';
         document.body.style.overflow = 'hidden';
@@ -17,17 +20,20 @@ export const modals = () => {
     });
 
     close.addEventListener('click', () => {
+      closeAllModals();
       closeModal();
     });
 
     modal.addEventListener('click', (event) => {
       if (event.target === modal) {
+        closeAllModals();
         closeModal();
       }
     });
 
     document.addEventListener('keydown', (event) => {
       if (event.key === 'Escape') {
+        closeAllModals();
         closeModal();
       }
     });
@@ -36,6 +42,12 @@ export const modals = () => {
       modal.style.display = 'none';
       document.body.style.overflow = '';
     };
+
+    const closeAllModals = () => {
+      windows.forEach(window => {
+        window.style.display = 'none';
+      });
+    }
   };
 
   const showModalByTime = (selector, time) => {
