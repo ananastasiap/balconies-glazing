cc4873dd1fff2132130873d74a6eee07016e6039
ananastasiap <anestheziia@gmail.com>
2023-04-05 10:44:59 +0300

    refactor: arguments turn into objects

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,5 +1,5 @@
 export const modals = () => {
-  const bindModal = (triggerSelector, modalSelector, closeSelector) => {
+  const bindModal = ({triggerSelector, modalSelector, closeSelector, closeClickOverlay = true}) => {
     const triggers = document.querySelectorAll(triggerSelector);
     const modal = document.querySelector(modalSelector);
     const close = document.querySelector(closeSelector);
@@ -25,7 +25,7 @@ export const modals = () => {
     });
 
     modal.addEventListener('click', (event) => {
-      if (event.target === modal) {
+      if (event.target === modal && closeClickOverlay) {
         closeAllModals();
         closeModal();
       }
@@ -57,9 +57,27 @@ export const modals = () => {
     }, time);
   };
 
-  bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
-  bindModal('.phone_link', '.popup', '.popup .popup_close');
-  bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
+  bindModal({
+    triggerSelector: '.popup_engineer_btn',
+    modalSelector: '.popup_engineer',
+    closeSelector: '.popup_engineer .popup_close'});
+  bindModal({
+    triggerSelector: '.phone_link',
+    modalSelector: '.popup',
+    closeSelector: '.popup .popup_close'});
+  bindModal({
+    triggerSelector: '.popup_calc_btn',
+    modalSelector: '.popup_calc',
+    closeSelector: '.popup_calc_close'});
+  bindModal ({
+    triggerSelector: '.popup_calc_button',
+    modalSelector: '.popup_calc_profile',
+    closeSelector: '.popup_calc_profile_close',
+    closeClickOverlay: 'false'});
+  bindModal ({
+    triggerSelector: '.popup_calc_profile_button',
+    modalSelector: '.popup_calc_end',
+    closeSelector: '.popup_calc_end_close',
+    closeClickOverlay: 'false'});
   showModalByTime('.popup', 60000);
-
 };
