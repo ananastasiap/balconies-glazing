bd617f0760ed5a3da2ae6aa3be23537ecc97398c
ananastasiap <anestheziia@gmail.com>
2023-04-18 01:33:24 +0300

    feat: hide scroll when modal is open

--- src/js/modules/images.js
+++ src/js/modules/images.js
@@ -1,4 +1,5 @@
 export const images = () => {
+  const rootElement = document.documentElement;
   const workSection = document.querySelector('.works');
   const imgPopup = document.createElement('div');
   const bigImage = document.createElement('img');
@@ -14,6 +15,7 @@ export const images = () => {
 
   const closeBigImg = () => {
     imgPopup.style.display = 'none';
+    rootElement.classList.remove('hide-scroll');
   }
 
   workSection.addEventListener('click', (event) => {
@@ -24,6 +26,7 @@ export const images = () => {
       imgPopup.style.display = 'flex';
       const path = target.parentNode.getAttribute('href');
       bigImage.setAttribute('src', path);
+      rootElement.classList.add('hide-scroll');
     }
 
     if (target && target.matches('div.popup')) {
